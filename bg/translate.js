b81f73a9926fedabce4e7b1f628b89491ce5c1d7
qidizi <qidizi@qq.com>
2020-03-31 09:46:15 +0800

    加入注入翻译时防错，暂无法重现

--- bg/translate.js
+++ bg/translate.js
@@ -2,27 +2,32 @@
 docker.browser.tabs.onUpdated.addListener(function (tid, change, ext) {
     // 加载中/完成状态，才注入
     //console.log(change.status, tid, ext.url)
-    if (
-        '.loading.complete.'.indexOf("." + change.status + '.') > -1
-        ||
-        !(/^(http|https|file|ftp):/i.test(ext.url))
-    )
+    if ('.loading.complete.'.indexOf("." + change.status + '.') > -1)
         return;
 
     docker.storage_local_get(docker.storage_keys.translate_on, (res) => {
         // 未启用
         if (!res.translate_on) return;
+        docker.browser.tabs.get(tid, function () {
+            if (docker.browser.runtime.lastError) {
+                // 这个错误只有在callback中有
+                console.log('tab异常，终止注入翻译功能：' + docker.browser.runtime.lastError.message);
+                return;
+            }
 
-        docker.browser.tabs.executeScript(tid, {
-            file: '/docker.js',
-            allFrames: true,
-            runAt: 'document_start' // 页面加载dom阶段插入
-        });
+            // 可能会出现 操作已经关闭的tab，触发
+            // Unchecked runtime.lastError: No tab with id: 1.
+            docker.browser.tabs.executeScript(tid, {
+                file: '/docker.js',
+                allFrames: true,
+                runAt: 'document_start' // 页面加载dom阶段插入
+            });
 
-        docker.browser.tabs.executeScript(tid, {
-            file: '/content/translate-client.js',
-            allFrames: true,
-            runAt: 'document_start'
+            docker.browser.tabs.executeScript(tid, {
+                file: '/content/translate-client.js',
+                allFrames: true,
+                runAt: 'document_start'
+            });
         });
     });
 });
